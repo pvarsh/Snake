c949107a8816f68c3c3c4a597708643119fdc5ae
pvarsh <pvarsh@users.noreply.github.com>
2023-05-30 04:48:49 +0000

    Ask ChatGPT to add player 2

--- script.js
+++ script.js
@@ -2,37 +2,41 @@ let grid = document.querySelector(".grid");
 let popup = document.querySelector(".popup");
 let playAgain = document.querySelector(".playAgain");
 let scoreDisplay = document.querySelector(".scoreDisplay");
-let left = document.querySelector(".left");
-let down = document.querySelector(".down");
-let right = document.querySelector(".right");
-let up = document.querySelector(".up");
 
 // Elements to display debugging info
 let snakeCoords = document.querySelector(".snakeCoords");
 let snakeDir = document.querySelector(".snakeDir");
 let keyDisplay = document.querySelector(".keyDisplay");
 
+const player1Controls = {
+  upKey: 38,
+  downKey: 40,
+  leftKey: 37,
+  rightKey: 39
+};
 
-const upKey = 38
-const downKey = 40
-const leftKey = 37
-const rightKey = 39
+const player2Controls = {
+  upKey: 87,  // W
+  downKey: 83,  // S
+  leftKey: 65,  // A
+  rightKey: 68  // D
+};
 
 let width = 10;
 let currentIndex = 0;
 let appleIndex = 0;
-let currentSnake = [2, 1, 0];
-let direction = 1;
-let score = 0;
+let snake1 = [2, 1, 0];
+let snake2 = [38, 39, 40];
+let direction1 = 1;
+let direction2 = -1;
+let score1 = 0;
+let score2 = 0;
 let speed = 0.8;
 let intervalTime = 0;
 let interval = 0;
 let directionChanged = false;
 
-document.onkeydown = checkKey;
-
 document.addEventListener("DOMContentLoaded", function() {
-  // document.addEventListener("keyup", control); // TODO: what is this for? Commenting out doesn't break anything immediately.
   createBoard();
   startGame();
   playAgain.addEventListener("click", replay);
@@ -50,63 +54,87 @@ function createBoard() {
 function startGame() {
   let squares = document.querySelectorAll(".grid div");
   randomApple(squares);
-  //random apple
-  direction = 1;
-  scoreDisplay.innerHTML = score;
+  direction1 = 1;
+  direction2 = -10;
+  score1 = 0;
+  score2 = 0;
+  scoreDisplay.innerHTML = "Player 1: " + score1 + " - Player 2: " + score2;
   intervalTime = 1000;
-  currentSnake = [2, 1, 0];
+  snake1 = [2, 1, 0];
+  snake2 = [79, 89, 99];
   currentIndex = 0;
-  currentSnake.forEach((index) => squares[index].classList.add("snake"));
+  snake1.forEach((index) => squares[index].classList.add("snake1"));
+  snake2.forEach((index) => squares[index].classList.add("snake2"));
   interval = setInterval(moveOutcome, intervalTime);
 }
 
 function moveOutcome() {
   let squares = document.querySelectorAll(".grid div");
   if (checkForHits(squares)) {
-    alert("you hit something");
+    alert("Game Over");
     popup.style.display = "flex";
     return clearInterval(interval);
   } else {
-    moveSnake(squares);
+    moveSnakes(squares);
   }
 }
 
-function moveSnake(squares) {
-  let tail = currentSnake.pop();
-  squares[tail].classList.remove("snake");
-  currentSnake.unshift(currentSnake[0] + direction);
+function moveSnakes(squares) {
+  moveSnake(squares, snake1, direction1, "snake1");
+  moveSnake(squares, snake2, direction2, "snake2");
+}
+
+function moveSnake(squares, snake, direction, snakeClass) {
+  let tail = snake.pop();
+  squares[tail].classList.remove(snakeClass);
+  snake.unshift(snake[0] + direction);
 
   // update debugging display
-  snakeCoords.textContent = currentSnake;
-  snakeDir.textContent = direction;
+  snakeCoords.textContent = "Player 1: " + snake1 + " - Player 2: " + snake2;
+  snakeDir.textContent = "Player 1: " + direction1 + " - Player 2: " + direction2;
 
-  // movement ends here
-  eatApple(squares, tail);
-  squares[currentSnake[0]].classList.add("snake");
+  eatApple(squares, snake, tail, snakeClass);
+  squares[snake[0]].classList.add(snakeClass);
 }
 
 function checkForHits(squares) {
   if (
-    (currentSnake[0] + width >= width * width && direction === width) ||
-    (currentSnake[0] % width === width - 1 && direction === 1) ||
-    (currentSnake[0] % width === 0 && direction === -1) ||
-    (currentSnake[0] - width <= 0 && direction === -width) ||
-    squares[currentSnake[0] + direction].classList.contains("snake")
+    (snake1[0] + width >= width * width && direction1 === width) ||
+    (snake1[0] % width === width - 1 && direction1 === 1) ||
+    (snake1[0] % width === 0 && direction1 === -1) ||
+    (snake1[0] - width <= 0 && direction1 === -width) ||
+    squares[snake1[0] + direction1].classList.contains("snake1") ||
+    squares[snake1[0] + direction1].classList.contains("snake2")
   ) {
     return true;
-  } else {
-    return false;
   }
+
+  if (
+    (snake2[0] + width >= width * width && direction2 === width) ||
+    (snake2[0] % width === width - 1 && direction2 === 1) ||
+    (snake2[0] % width === 0 && direction2 === -1) ||
+    (snake2[0] - width <= 0 && direction2 === -width) ||
+    squares[snake2[0] + direction2].classList.contains("snake2") ||
+    squares[snake2[0] + direction2].classList.contains("snake1")
+  ) {
+    return true;
+  }
+
+  return false;
 }
 
-function eatApple(squares, tail) {
-  if (squares[currentSnake[0]].classList.contains("apple")) {
-    squares[currentSnake[0]].classList.remove("apple");
-    squares[tail].classList.add("snake");
-    currentSnake.push(tail);
+function eatApple(squares, snake, tail, snakeClass) {
+  if (squares[snake[0]].classList.contains("apple")) {
+    squares[snake[0]].classList.remove("apple");
+    squares[tail].classList.add(snakeClass);
+    snake.push(tail);
     randomApple(squares);
-    score++;
-    scoreDisplay.textContent = score;
+    if (snakeClass === "snake1") {
+      score1++;
+    } else {
+      score2++;
+    }
+    scoreDisplay.textContent = "Player 1: " + score1 + " - Player 2: " + score2;
     clearInterval(interval);
     intervalTime = intervalTime * speed;
     interval = setInterval(moveOutcome, intervalTime);
@@ -118,20 +146,11 @@ function eatApple(squares, tail) {
 function randomApple(squares) {
   do {
     appleIndex = Math.floor(Math.random() * squares.length);
-  } while (squares[appleIndex].classList.contains("snake"));
+  } while (
+    squares[appleIndex].classList.contains("snake1") ||
+    squares[appleIndex].classList.contains("snake2")
+  );
   squares[appleIndex].classList.add("apple");
-}
-
-function control(e) {
-  if (e.keycode === 39) {
-    direction = 1; // right
-  } else if (e.keycode === 38) {
-    direction = -width; //if we press the up arrow, the snake will go ten divs up
-  } else if (e.keycode === 37) {
-    direction = -1; // left, the snake will go left one div
-  } else if (e.keycode === 40) {
-    direction = +width; // down the snake head will instantly appear 10 divs below from the current div
-  }
 }
 
 function replay() {
@@ -141,34 +160,42 @@ function replay() {
   popup.style.display = "none";
 }
 
-
-
-function checkKey(e) {
+document.addEventListener("keydown", function(e) {
   keyDisplay.textContent = e.keyCode;
   switch (e.keyCode) {
-    case upKey:
-      direction = -width;
+    case player1Controls.upKey:
+      direction1 = -width;
       keyDisplay.textContent = keyDisplay.textContent + " ( ↑ )";
       break;
-    case downKey:
-      direction = width;
+    case player1Controls.downKey:
+      direction1 = width;
       keyDisplay.textContent = keyDisplay.textContent + " ( ↓ )";
       break;
-    case leftKey:
-      direction = -1;
+    case player1Controls.leftKey:
+      direction1 = -1;
       keyDisplay.textContent = keyDisplay.textContent + " ( ← )";
       break;
-    case rightKey:  
-      direction = +1;
+    case player1Controls.rightKey:
+      direction1 = 1;
+      keyDisplay.textContent = keyDisplay.textContent + " ( → )";
+      break;
+    case player2Controls.upKey:
+      direction2 = -width;
+      keyDisplay.textContent = keyDisplay.textContent + " ( ↑ )";
+      break;
+    case player2Controls.downKey:
+      direction2 = width;
+      keyDisplay.textContent = keyDisplay.textContent + " ( ↓ )";
+      break;
+    case player2Controls.leftKey:
+      direction2 = -1;
+      keyDisplay.textContent = keyDisplay.textContent + " ( ← )";
+      break;
+    case player2Controls.rightKey:
+      direction2 = 1;
       keyDisplay.textContent = keyDisplay.textContent + " ( → )";
       break;
     default:
-      console.log("Key is not supported")
+      console.log("Key is not supported");
   }
-}
-
-up.addEventListener("click", () => (direction = -width));
-down.addEventListener("click", () => (direction = +width));
-left.addEventListener("click", () => (direction = -1));
-right.addEventListener("click", () => (direction = 1));
-
+});
